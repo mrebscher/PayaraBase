9b22aeb3a145740d2a9fba6b4c73e61e1d528512
Gosia <gosia_maciejewska@tlen.pl>
2016-07-03 19:20:45 +0200

    Reworked how Payara is packaged

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 docker build \
-  -t mrebscher/payarabase:latest \
-  -t mrebscher/payarabase:4.1.1.154 \
-  -t mrebscher/payarabase:4.1.1 \
+  -t mrebscher/payara-base:latest \
+  -t mrebscher/payara-base:4.1.1.154 \
+  -t mrebscher/payara-base:4.1.1 \
   .
